ea6ee55ad845d86501b7f42ac0c1033a4172487b
Darlan Cavalcante Moreira <darcamo@gmail.com>
2017-05-12 19:41:03 -0300

    Initial presentation commit

---
+++ js/add_Reveal_Event_Listeners.js
@@ -0,0 +1,38 @@
+// Veja de onde você tiroi isso em
+// https://github.com/ptamarit/slides-data-viz-web-d3/blob/master/src/js/reveal-to-d3.js
+Reveal.addEventListener('slidechanged', function(event) {
+    update_baterias();
+    // update_ipython_magic();
+    // update_indexa_array();
+    // update_indexa_array2();
+    // update_indexa_array3();
+    // update_manipulando_o_shape();
+    // update_C_order();
+    // update_fortran_order();
+    // update_broadcast();
+    // update_axis_params();
+});
+Reveal.addEventListener('fragmentshown', function(event) {
+    update_baterias();
+    // update_ipython_magic();
+    // update_indexa_array();
+    // update_indexa_array2();
+    // update_indexa_array3();
+    // update_manipulando_o_shape();
+    // update_C_order();
+    // update_fortran_order();
+    // update_broadcast();
+    // update_axis_params();
+});
+Reveal.addEventListener('fragmenthidden', function(event) {
+    update_baterias();
+    // update_ipython_magic();
+    // update_indexa_array();
+    // update_indexa_array2();
+    // update_indexa_array3();
+    // update_manipulando_o_shape();
+    // update_C_order();
+    // update_fortran_order()
+    // update_broadcast();
+    // update_axis_params();
+});
